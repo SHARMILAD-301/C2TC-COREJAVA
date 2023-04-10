87c92bb4d29fa48d61b04cb30e28ae3efbfb9f28
sharm <sharm@host.docker.internal>
2023-04-10 13:43:50 +0530

    commit three

---
+++ CAP/src/AccessModifier/Student1.java
@@ -0,0 +1,18 @@
+package AccessModifier;
+
+public class Student1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Students s1 = new Students(111,"Sharmila");  
+	    Students s2 = new Students(222,"Vennila");  
+	    Students s3 = new Students(333,"Glory"); 
+	    Students s4 = new Students(444,"Nila");
+	    //calling method to display the values of object  
+	    s1.display();  
+	    s2.display(); 
+	    s3.display();
+	    s4.display();
+	   }  
+	}
+
---
+++ CAP/src/AccessModifier/Students.java
@@ -0,0 +1,17 @@
+package AccessModifier;
+
+public class Students { 
+	    int id;  
+	    String name;  
+	    //creating a parameterized constructor  
+	    Students (int i,String n){  
+	    id = i;  
+	    name = n;  
+	    }  
+	    //method to display the values  
+	    void display()
+	    {
+	    	System.out.println(id+" "+name);
+	    	}  
+	    
+}
